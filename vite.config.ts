cfa5ffe636a39403d846547eaf0d952b925a368f
ahmed-abd-alalim <ahmedabdalalim.3a@gmail.com>
2025-06-13 23:07:00 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+    resolve: {
+    alias: {
+      '@':"src",
+      '@components': "src/components",
+      '@layouts': "src/layouts",
+      '@types': "src/types",
+      '@pages': "src/pages",
+      '@config': "src/config",
+    },
+  },
+})
