ad0fd4c4530691f7318cca997e250d338446060f
ahmed-abd-alalim <ahmedabdalalim.3a@gmail.com>
2025-06-14 22:42:20 +0300

    #

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-    resolve: {
-    alias: {
-      '@': path.resolve(__dirname,"src"),
-      "@storage": path.resolve(__dirname, "storage"),
-      '@components': path.resolve(__dirname,"src/components"),
-      '@layouts': path.resolve(__dirname,"src/layouts"),
-      '@types': path.resolve(__dirname,"src/types"),
-      '@pages': path.resolve(__dirname,"src/pages"),
-      '@config': path.resolve(__dirname,"src/config"),
-    },
-  },
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 })
