60348522d0ff5fb3abd16560477d60fd28940982
ahmed-abd-alalim <ahmedabdalalim.3a@gmail.com>
2025-06-17 05:20:29 +0300

    #

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,26 +2,26 @@
 // --> stete context
 // ################
 export type props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 export type productsType = {
-    img: string,
-    name: string,
-    title: string,
-    price: number
-  }[]
+  img: string;
+  name: string;
+  title: string;
+  price: number;
+}[];
 
 export type reviewsType = {
-    img:string,
-    name: string,
-    stars: number,
-    review: string
-  }[]
+  img: string;
+  name: string;
+  stars: number;
+  review: string;
+}[];
 
 export type stateType = {
- products:  productsType
- setProducts : React.Dispatch<React.SetStateAction<productsType>>
- reviews:  reviewsType
- setReviews : React.Dispatch<React.SetStateAction<reviewsType>>
-}
+  products: productsType;
+  setProducts: React.Dispatch<React.SetStateAction<productsType>>;
+  reviews: reviewsType;
+  setReviews: React.Dispatch<React.SetStateAction<reviewsType>>;
+};
